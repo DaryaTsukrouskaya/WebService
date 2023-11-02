86740c98e591ba50d37e0a26ffad1018171a6201
Darya Tsukrovskaya <darya.raikhert.31@mail.ru>
2023-11-03 00:36:08 +0300

    some changes

--- src/main/resources/db.migration/V2__addProducts.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Классические колготки","Полупрозрачные Колготки","tights.png",5,1119);
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Плотные колготки","Колготки из Модала с Добавлением Кашемира","warmTights.png",5,1119);
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Чулки","Прозрачные Матовые чулки","stockings.png",5,1419);
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Теплые чулки","Шерстяные Гольфины","warmStockings.png",5,1119);
-
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Классические носки","Носки с Блесткми","socks.png",6,199);
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Короткие носки","Короткие Носки изо Льна и Вискозы","shortSocks.png",6,199);
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Прозрачные носки","Ультратонкие Носки","transparentSocks.png",6,199);
-INSERT INTO shop.products(name,description,imagePath,categoryId,price) VALUES("Следки","Следки изо Льна и Вискозы","footprints.png",6,199);
